263ace7796c968bc70cde1e0ce50a451292e9fca
Eliel <elielbianchi@hotmail.com>
2022-03-03 12:08:20 -0300

    Refatoração das classes de teste dos ex 1 e 2

--- exercicio1/TesteMediana.java
+++ exercicio1/TesteMediana.java
@@ -2,19 +2,26 @@ package exercicio1;
 
 public class TesteMediana {
     public static void main(String[] args) {
+
+        // Bloco de entrada para testes, alterar aqui para testar novas combinações
         int[] lista1 = {9,1,3,18,11,22,7};
+        int esperado1 = 9;
         int[] lista2 = {9,2,1,4,6};
+        int esperado2 = 4;
         int[] lista3 = {2,7,10,1,15,9,5,5,12};
+        int esperado3 = 7;
+
+        // Bloco de execução do teste, não modificar aqui
         Mediana mediana1 = new Mediana(lista1);
         Mediana mediana2 = new Mediana(lista2);
         Mediana mediana3 = new Mediana(lista3);
         System.out.println("Teste do exercício 1: Mediana");
-        System.out.println("Teste 1:");
-        System.out.println(testarNumero(mediana1.calculaMediana(), 9));
-        System.out.println("Teste 2:");
-        System.out.println(testarNumero(mediana2.calculaMediana(), 4));
-        System.out.println("Teste 3:");
-        System.out.println(testarNumero(mediana3.calculaMediana(), 7));
+        System.out.println("Teste 1 - Primeira lista:");
+        System.out.println(testarNumero(mediana1.calculaMediana(), esperado1));
+        System.out.println("Teste 2 - Segunda lista:");
+        System.out.println(testarNumero(mediana2.calculaMediana(), esperado2));
+        System.out.println("Teste 3: Terceira lista:");
+        System.out.println(testarNumero(mediana3.calculaMediana(), esperado3));
     }
 
     public static String testarNumero(int resultado, int esperado) {
--- exercicio2/TesteVetor.java
+++ exercicio2/TesteVetor.java
@@ -5,23 +5,26 @@ public class TesteVetor {
         
         // Bloco de entrada para testes, alterar aqui para testar novas combinações
         int[] vetor1 = {1,5,2,8,11,7,3};
+        int diferenca1 = 3;
         int esperado1 = 3;
         int[] vetor2 = {1,5,3,4,2};
-        int esperado2 = 2;
+        int diferenca2 = 2;
+        int esperado2 = 3;
         int[] vetor3 = {2,7,10,1,11,9,5,3,12};
-        int esperado3 = 1;
+        int diferenca3 = 1;
+        int esperado3 = 5;
         
-        // Execução do teste, não modificar aqui
-        Vetor vetorA = new Vetor(vetor1, esperado1);
-        Vetor vetorB = new Vetor(vetor2, esperado2);
-        Vetor vetorC = new Vetor(vetor3, esperado3);
+        // Bloco de execução do teste, não modificar aqui
+        Vetor vetorA = new Vetor(vetor1, diferenca1);
+        Vetor vetorB = new Vetor(vetor2, diferenca2);
+        Vetor vetorC = new Vetor(vetor3, diferenca3);
         System.out.println("Teste do exercício 2: Número de elementos no vetor com diferença");
-        System.out.println("Teste 1:");
-        System.out.println(testarNumero(vetorA.numeroParesElementos(), 3));
-        System.out.println("Teste 2:");
-        System.out.println(testarNumero(vetorB.numeroParesElementos(), 3));
-        System.out.println("Teste 3:");
-        System.out.println(testarNumero(vetorC.numeroParesElementos(), 5));
+        System.out.println("Teste 1 - Pares de elementos com diferença de " + diferenca1);
+        System.out.println(testarNumero(vetorA.numeroParesElementos(), esperado1));
+        System.out.println("Teste 2 - Pares de elementos com diferença de " + diferenca2);
+        System.out.println(testarNumero(vetorB.numeroParesElementos(), esperado2));
+        System.out.println("Teste 3 - Pares de elementos com diferença de " + diferenca3);
+        System.out.println(testarNumero(vetorC.numeroParesElementos(), esperado3));
     }
 
     public static String testarNumero(int resultado, int esperado) {
