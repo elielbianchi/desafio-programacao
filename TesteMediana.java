7b87c9f81540a26059993ad6ce6951fda4eea7c6
Eliel <elielbianchi@hotmail.com>
2022-03-03 10:40:53 -0300

    Refatoração teste classe Mediana

--- TesteMediana.java
+++ TesteMediana.java
@@ -3,22 +3,16 @@ public class TesteMediana {
         int[] lista1 = {9,1,3,18,11,22,7};
         int[] lista2 = {9,2,1,4,6};
         int[] lista3 = {2,7,10,1,15,9,5,5,12};
-        int esperado1 = 9;
-        int esperado2 = 4;
-        int esperado3 = 7;
         Mediana mediana1 = new Mediana(lista1);
         Mediana mediana2 = new Mediana(lista2);
         Mediana mediana3 = new Mediana(lista3);
-        int resultado1 = mediana1.calculaMediana();
-        int resultado2 = mediana2.calculaMediana();
-        int resultado3 = mediana3.calculaMediana();
         System.out.println("Teste do exercício 1: Mediana");
         System.out.println("Teste 1:");
-        System.out.println(testarNumero(resultado1, esperado1));
+        System.out.println(testarNumero(mediana1.calculaMediana(), 9));
         System.out.println("Teste 2:");
-        System.out.println(testarNumero(resultado2, esperado2));
+        System.out.println(testarNumero(mediana2.calculaMediana(), 4));
         System.out.println("Teste 3:");
-        System.out.println(testarNumero(resultado3, esperado3));
+        System.out.println(testarNumero(mediana3.calculaMediana(), 7));
     }
 
     public static String testarNumero(int resultado, int esperado) {
