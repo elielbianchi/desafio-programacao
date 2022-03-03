c79a5069e89b98f2ece74ae417b2de29570b949d
Eliel <elielbianchi@hotmail.com>
2022-03-03 14:04:14 -0300

    Criada a classe do exercicio 3

---
+++ exercicio3/Encriptacao.java
@@ -0,0 +1,47 @@
+package exercicio3;
+
+public class Encriptacao {
+    private String fraseOriginal;
+    private String fraseEncriptada;
+    private int tamanhoTexto;
+    private int limiteInferior;
+    private int limiteSuperior;
+    private int colunas;
+
+    public Encriptacao(String frase) {
+        fraseOriginal = frase.replace(" ","");
+        tamanhoTexto = frase.length();
+        limiteInferior = (int)Math.pow(tamanhoTexto,0.5);
+        limiteSuperior = limiteInferior + 1;
+    }
+
+    public String encriptar() {
+        definirLinhasColunas();
+        fraseEncriptada = modificarFrase();
+        return fraseEncriptada;
+    }
+
+    private void definirLinhasColunas() {
+        if(tamanhoTexto == limiteInferior*limiteInferior) {
+            colunas = limiteInferior;
+        } else {
+            colunas = limiteSuperior;
+        }
+    }
+
+    private String modificarFrase () {
+        String modificacao = "";
+        
+        for(int coluna = 0; coluna < colunas; coluna++) {
+            
+            for(int linha = coluna; linha < tamanhoTexto; linha += colunas) {
+                modificacao = modificacao + fraseOriginal.charAt(linha);
+            }
+            
+            if(coluna+1 < colunas) {
+                modificacao = modificacao + " ";
+            }
+        }
+        return modificacao;
+    }
+}
